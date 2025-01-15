36bce76dc865ee1eaa7d6fa5865456f921499a98
JJ <triplej12.business@gmail.com>
2025-01-15 16:53:33 -0400

    admin.php fixes for inserts
    added insert function to send info to db, only firstname, lastname, email, and role for now, but working

--- connect.php
+++ connect.php
@@ -1,15 +1,13 @@
 <?php
-//these are defined as constants
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'monctondbconnection.redirectme.net');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-//define('DB_NAME', 'productsdemo');
+define('DB_PASS', 'MonctonDB2024094');
 define('DB_NAME', 'monctondb');
-	
+    
 global $con;
-	  $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS, DB_NAME);
-if (!$con)
-  {
-  die('Could not connect: '); //. mysql_error());
-  }
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (!$con) {
+    die('Could not connect: ' . mysqli_connect_error());
+}
 ?>
