48df4db90007d70656bdc59137ec1cb49c871562
JJ <triplej12.business@gmail.com>
2025-01-20 11:13:43 -0400

    comments on connect.php file
    connect.php file comments updated

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST', 'monctondbconnection.redirectme.net');
-define('DB_USER', 'root');
-define('DB_PASS', 'MonctonDB2024094');
+define('DB_HOST', 'monctondbconnection.redirectme.net'); //or localhost if working  on mysql
+define('DB_USER', 'root'); 
+define('DB_PASS', 'MonctonDB2024094'); //or blank if working locally on mysql
 define('DB_NAME', 'monctondb');
     
 global $con;
