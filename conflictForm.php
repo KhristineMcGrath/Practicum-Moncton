05a5f757bbe3d930e81bae199b2742fb5d0bd05d
KhristineMcGrath <152993610+KhristineMcGrath@users.noreply.github.com>
2025-02-06 21:39:08 -0400

    Conflict Form update
    Polished some buttons and additional questions back from BAs that needed to be added. This form will have a few commits.

--- conflictForm.php
+++ conflictForm.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conflict Report</title>
     <link rel="stylesheet" href="includes/conflictForm.css">
+    <script>
+       function addTextbox(containerId) {
+            const container = document.getElementById(containerId);
+            const newDiv = document.createElement("div"); // Wrapper div for each textbox + button
+            
+            newDiv.innerHTML = `
+                <input type="text" name="textboxes[]" placeholder="Enter text" required>
+                <button type="button" onclick="removeTextbox(this)">Remove</button>
+            `;
+
+            container.appendChild(newDiv);
+        }
+
+        function removeTextbox(button) {
+            button.parentNode.remove(); // Remove the textbox div
+        }
+    </script>
 </head>
+
 <body>
     <form>
         <div class="container">
@@ -20,7 +39,7 @@
                         <input type="text" name="last_name" placeholder="Last Name">
                     </div>
                     <div class="section">
-                        <label>Start of Conflict</label> 
+                        <label>Start of Conflict</label>
                         <input type="time" name="day">
                         <label>Date</label>
                         <input type="date" name="date">
@@ -34,8 +53,8 @@
                         <input type="text" name="staff_last_name" placeholder="Staff Last Name">
                     </div>
                     <div class="section">
-                    <label>End of Conflict</label> <!-- Look at length of conflict for time section. -->
-                    <input type="time" name="day">
+                        <label>End of Conflict</label> <!-- Look at length of conflict for time section. -->
+                        <input type="time" name="day">
                     </div>
                 </div>
             </div>
@@ -44,13 +63,15 @@
              did not fix the setting of the grids. -->
             <div class="bottom-section">
                 <div class="section">
-                    <label>Injuries & Damages</label>
+                    <label>Injuries & Damages?</label>
                     <input type="text" name="injuries">
                 </div>
                 <div class="section">
-                    <label>Estimated Cost of Damage</label>
+                    <label>Estimated Cost of Damages?</label>
                     <input type="text" name="cost">
                 </div>
+                <button type="button" onclick="addTextbox('section1')">Add Row</button> <br>
+                <div id="section1"></div><br>
                 <div class="section">
                     <label>Police Involved?</label>
                     <div class="radio-group">
@@ -59,9 +80,11 @@
                     </div>
                 </div>
                 <div class="section">
-                    <label>Officer Name</label>
+                    <label>Officer Name(s):</label>
                     <input type="text" name="officer_name">
                 </div>
+                <button type="button" onclick="addTextbox('section2')">Add Row</button> <br>
+                <div id="section2"></div><br>
                 <div class="section">
                     <label>Social Worker Contacted?</label>
                     <div class="radio-group">
@@ -74,6 +97,8 @@
                     <input type="text" name="social_worker_name">
                 </div>
             </div>
+            <button type="button" onclick="addTextbox('section3')">Add Row</button> <br>
+                <div id="section3"></div><br>
             <br>
             <!-- Row section for across is fine for now -->
             <div class="row">
@@ -81,27 +106,33 @@
                 <textarea id="contact_reason" name="contact_reason" rows="2"></textarea>
             </div>
             <div class="row">
-                <label for="observations">What you observed:</label>
+                <label for="observations">What was observed:</label>
                 <textarea id="observations" name="observations" rows="2"></textarea>
             </div>
             <div class="row">
                 <label for="actions">Actions of upset person:</label>
                 <textarea id="actions" name="actions" rows="2"></textarea>
             </div>
+
+            <!-- ## MIN options offered should be two, but other sections to add more. -->
             <div class="row">
                 <label for="options">Options offered:</label>
                 <textarea id="options" name="options" rows="2"></textarea>
             </div>
+            <button type="button" onclick="addTextbox('section4')">Add Row</button> <br>
+                <div id="section4"></div>
             <div class="row">
                 <label for="support">What was done to support the upset person?</label>
                 <textarea id="support" name="support" rows="2"></textarea>
             </div>
+            <button type="button" onclick="addTextbox('section5')">Add Row</button> <br>
+            <div id="section5"></div><br>
             <div class="row">
                 <label for="consequences">Logical consequences:</label>
                 <textarea id="consequences" name="consequences" rows="2"></textarea>
             </div>
             <br>
-            <!-- Adjust spacing between radio buttons and labels -->
+            <!-- Adjust spacing between radio buttons and label, add borders as an option? Look into color scheme.-->
             <div class="section">
                 <label>Is everyone okay physically and emotionally?</label>
                 <div class="radio-group">
@@ -110,18 +141,33 @@
                 </div>
             </div>
             <div class="section">
-                <label>Question 2 ## Check with BA's'?</label>
+                <label>Did we follow the person's support plan?</label>
                 <div class="radio-group">
-                    <input type="radio" id="okay_yes" name="emotion" value="Yes"> Yes
-                    <input type="radio" id="okay_no" name="emotion" value="No"> No
-            </div>
+                    <input type="radio" id="okay_yes" name="supportplan" value="Yes"> Yes
+                    <input type="radio" id="okay_no" name="supportplan" value="No"> No
+                </div>
             </div>
             <div class="section">
-                <label>Question 3 ## Check with BA's'?</label>
+                <label>Was a Team Leader identified and did he/she direct and communicate to all?</label>
+                <div class="radio-group">
+                    <input type="radio" id="okay_yes" name="teamlead" value="Yes"> Yes
+                    <input type="radio" id="okay_no" name="teamlead" value="No"> No
+                </div>
+            </div>
+            <div class="section">
+                <label>Did we choose the safest option?</label>
                 <div class="radio-group">
                     <input type="radio" id="okay_yes" name="emotion" value="Yes"> Yes
                     <input type="radio" id="okay_no" name="emotion" value="No"> No
+                </div>
             </div>
+            <div class="row">
+                <label for="members">What did my team members do well:</label>
+                <textarea id="members" name="members" rows="2"></textarea>
+            </div>
+            <div class="row">
+                <label for="changes">What changes would we like to see next time?</label>
+                <textarea id="changes" name="changes" rows="2"></textarea>
             </div>
             <div class="row">
                 <label for="signature">Signature:</label>
@@ -133,4 +179,5 @@
         </div>
     </form>
 </body>
+
 </html>
